fb6aa2afc521dbcbc6a4c88b12070933f52bf1e0
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2022-12-13 01:13:41 -0300

    chore: removing unused log

--- src/utils/thousandPlusFormatter.ts
+++ src/utils/thousandPlusFormatter.ts
@@ -1,6 +1,5 @@
 export function ThousandPlusFormatter(text: string){
   const valueParsed = parseInt(text)
-  console.log(valueParsed > 999)
     if(valueParsed > 999){
       return `${text[0]}k+`
     }
